3a37743b48ae103a644d579f956f3481fef029e0
Connor Anderson <connoranderson@Connors-MBP.home>
2019-07-27 22:42:44 -0400

    Documentation and print statements to report status

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import praw
 import time
 import re
 
-#
+#creates an instance of a reddit account
 reddit = praw.Reddit(client_id = 'pVhsuEKtvSeNVQ',
                      client_secret ='y1slipowOeHK5UTjLvby26PKwnA',
                      username='Cornell_class_Bot',
@@ -12,42 +12,45 @@ reddit = praw.Reddit(client_id = 'pVhsuEKtvSeNVQ',
 
 subreddit = reddit.subreddit('Cornell')
 
+#declares neccesary global variables
 checkedPosts = []
-
 count = 0
-
+comment = 0
 output = ""
 
+#common 4-digit numbers mentioned in posts that could create a false-positive
 invalidNums = ['1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1999', '2000', '2001', '2002', '2003',
                '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
                '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '1000', '3000', '4000',
                '5000', '6000']
 
-def get_date(submission):
-	time = submission.created
-	return datetime.datetime.fromtimestamp(time)
-
+#removes duplicate numbers from lists. Used incase the class number is mentioned multiple times in a post
 def remove_duplicates(x):
     return list(set(x))
 
+#removes the commonly mentioned 4-digit numbers listed above from the numbers detected to be classes
 def remove_nums(x):
     for num in x:
         if num in invalidNums:
             x.remove(num)
     return x
 
+#finds posts containing the same class mentioned in the original post by ensuring the class number appears in the
+#to-be recommended post's title and comment section
 def find_posts(classes, url, num):
     output = ""
+    print("Searching for related material")
     for archive in reddit.subreddit('cornell').new(limit = None):
         for classNums in classes:
             if classNums in archive.title + " " + archive.selftext:
                 for comment in archive.comments:
                     if classNums in comment.body and archive.url != url and archive.url not in output and num < 5:
-                        output = output + archive.url + "\n"
+                        output = output + archive.url + "\n\n"
                         num = num + 1
                         break
     return output
 
+#loop to constantly check the most recent 25 posts to see if it's neccesary for the bot to comment
 while 1 == 1:
     for submission in reddit.subreddit('cornell').new(limit=25):
         keyphrase = remove_nums(remove_duplicates(re.findall(r'\d{4}', submission.title + " " + submission.selftext)))
@@ -58,6 +61,9 @@ while 1 == 1:
             output = find_posts(keyphrase, submission.url, count)
             count = 0
         if len(output) != 0:
-            #submission.comment("I noticed you asked about a specific class! Here are some possibly useful links: \n" + output)
+            submission.reply("I noticed you asked about a specific class! Here are some possibly useful links: \n\n" + output)
+            comment = comment + 1
+            print("This is the " + comment + "th comment!")
             output = ""
+    print("Sleeping now")
     time.sleep(300)
